907e62aca3ecc90f3b83962fba4ff73a540a6222
Iris <iris.vilaseca1998@gmail.com>
2022-04-27 10:17:26 +0200

    first commit

---
+++ src/main/java/com/springers/techevents/TecheventsApplication.java
@@ -0,0 +1,13 @@
+package com.springers.techevents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecheventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecheventsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springers/techevents/TecheventsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springers.techevents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecheventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
