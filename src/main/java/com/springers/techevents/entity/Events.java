dff89e726550ab87bad7f41042ac9bdaddbac13c
verodev <verodev@verodevs-MacBook-Air.local>
2022-04-27 12:40:29 +0200

    added event properties

--- src/main/java/com/springers/techevents/entity/Events.java
+++ src/main/java/com/springers/techevents/entity/Events.java
@@ -1,4 +1,26 @@
 package com.springers.techevents.entity;
 
-public class Events {
+import javax.persistence.*;
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+public class Events implements Serializable
+{
+    @Serial
+    private final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private boolean featured;
+
+    private int max_participants;
+
+    private String name, date, description, image;
+
+    @ManyToMany(mappedBy = "events")
+    private List<Users> users;
 }
