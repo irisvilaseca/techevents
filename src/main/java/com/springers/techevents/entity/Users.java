f066adcdd7838420cc48d5d8265377d37b33fc05
Anitameri <la_petite_de_la_famille@hotmail.com>
2022-04-27 14:03:20 +0200

    create properties and table/Users

--- src/main/java/com/springers/techevents/entity/Users.java
+++ src/main/java/com/springers/techevents/entity/Users.java
@@ -1,4 +1,24 @@
 package com.springers.techevents.entity;
 
+
+import net.bytebuddy.dynamic.loading.InjectionClassLoader;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name = "")
 public class Users {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String usuario;
+    private String contraseña;
+
+    @ManyToMany
+    @JoinColumn(name = "")
+    private Events events;
+
 }
