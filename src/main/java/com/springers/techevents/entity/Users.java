c55ca041791d70c0d74dcb693a4cd86b21ace233
Anitameri <la_petite_de_la_famille@hotmail.com>
2022-04-27 14:28:09 +0200

    change table and create properties

--- src/main/java/com/springers/techevents/entity/Users.java
+++ src/main/java/com/springers/techevents/entity/Users.java
@@ -4,18 +4,22 @@ package com.springers.techevents.entity;
 import net.bytebuddy.dynamic.loading.InjectionClassLoader;
 
 import javax.persistence.*;
+import java.io.Serial;
+import java.io.Serializable;
 
 
 @Entity
 @Table(name = "")
-public class Users {
+public class Users implements Serializable {
+
+    @Serial private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Long id;
     private String email;
     private String usuario;
-    private String contraseña;
+    private String contrasena;
 
     @ManyToMany
     @JoinColumn(name = "")
