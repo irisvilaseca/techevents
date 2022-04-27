8d2644607906abf0d0315742437493c4fbd48a00
verodev <verodev@verodevs-MacBook-Air.local>
2022-04-27 14:18:43 +0200

    updated getters for user

--- src/main/java/com/springers/techevents/entity/Users.java
+++ src/main/java/com/springers/techevents/entity/Users.java
@@ -4,6 +4,9 @@ package com.springers.techevents.entity;
 import net.bytebuddy.dynamic.loading.InjectionClassLoader;
 
 import javax.persistence.*;
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.List;
 
 
 @Entity
@@ -18,7 +21,13 @@ public class Users {
     private String contraseña;
 
     @ManyToMany
-    @JoinColumn(name = "")
     private Events events;
 
+    public Long getId (){return id;}
+
+    public String getEmail(){return email;}
+
+    public String getUser(){return usuario;}
+
+    public String getPassword(){return contraseña;}
 }
