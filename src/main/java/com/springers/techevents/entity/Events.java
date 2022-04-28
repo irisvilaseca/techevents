49e55837a2b8efbdfc542d8b0871b90721f17579
Iris <iris.vilaseca1998@gmail.com>
2022-04-28 09:04:37 +0200

    events getters and setters

--- src/main/java/com/springers/techevents/entity/Events.java
+++ src/main/java/com/springers/techevents/entity/Events.java
@@ -9,7 +9,7 @@ import java.util.List;
 public class Events implements Serializable
 {
     @Serial
-    private final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,4 +23,72 @@ public class Events implements Serializable
 
     @ManyToMany(mappedBy = "events")
     private List<Users> users;
+
+    public long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public boolean isFeatured() {
+        return featured;
+    }
+
+    public void setFeatured(boolean featured) {
+        this.featured = featured;
+    }
+
+    public int getMax_participants() {
+        return max_participants;
+    }
+
+    public void setMax_participants(int max_participants) {
+        this.max_participants = max_participants;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public List<Users> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<Users> users) {
+        this.users = users;
+    }
 }
