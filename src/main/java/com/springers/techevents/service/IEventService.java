9b3c05afd319228e97a32f5540a6bcc2d9fd9685
Anitameri <la_petite_de_la_famille@hotmail.com>
2022-04-28 12:29:31 +0200

    IEventSservice

--- src/main/java/com/springers/techevents/service/IEventService.java
+++ src/main/java/com/springers/techevents/service/IEventService.java
@@ -1,4 +1,14 @@
 package com.springers.techevents.service;
 
+
+import java.util.List;
+import com.springers.techevents.entity.Events;
+
 public interface IEventService {
+
+
+    public List<Events> listarTodos();
+    public void guardar(Events event);
+    public Events bucarPorId(Long id);
+    public void eliminar(Long id);
 }
