dafa2a717f4e99387b0773fda226f2d86ee25a11
verodev <verodev@verodevs-MacBook-Air.local>
2022-05-04 09:49:52 +0200

    event controller get mapping

--- src/main/java/com/springers/techevents/controller/EventController.java
+++ src/main/java/com/springers/techevents/controller/EventController.java
@@ -1,5 +1,13 @@
 package com.springers.techevents.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class EventController {
-    .
+    @GetMapping("/views/events")
+    public String load()
+    {
+        return "";
+    }
 }
