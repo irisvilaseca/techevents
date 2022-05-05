9ffbd790aed13b54559c88d825ccaa029e9f60bc
Iris <iris.vilaseca1998@gmail.com>
2022-05-05 15:23:59 +0200

    post function working

--- src/main/java/com/springers/techevents/controller/EventController.java
+++ src/main/java/com/springers/techevents/controller/EventController.java
@@ -21,8 +21,11 @@ public class EventController {
     private EventServiceImpl eventService;
     private UserServiceImpl service;
     @GetMapping("/events")
-    public String load()
+    public String load(Model model)
     {
+       Events evento1 = eventService.buscarPorId(1l);
+        System.out.println(evento1.toString());
+        model.addAttribute("eventoPrueba",evento1);
         return "views/events/singleEvent";
     }
     @PostMapping("/events/create")
--- src/main/java/com/springers/techevents/repository/EventRepository.java
+++ src/main/java/com/springers/techevents/repository/EventRepository.java
@@ -1,9 +1,10 @@
 package com.springers.techevents.repository;
 
 import com.springers.techevents.entity.Events;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 @Repository
-public interface EventRepository extends CrudRepository <Events, Long> {
+public interface EventRepository extends JpaRepository<Events, Long> {
 
 }
